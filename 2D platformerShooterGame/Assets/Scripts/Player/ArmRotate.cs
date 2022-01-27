024725ea9f572186c25c661e87a86f55f84d9d67
Orhun Kaan Çağlayan <66566691+Orhunxx@users.noreply.github.com>
2022-01-27 22:19:58 +0300

    ArmRotating

--- 2D platformerShooterGame/Assets/Scripts/Player/ArmRotate.cs
+++ 2D platformerShooterGame/Assets/Scripts/Player/ArmRotate.cs
@@ -26,6 +26,8 @@ public class ArmRotate : MonoBehaviour
     {
         Vector3 mousePos = new Vector3(cam.ScreenToWorldPoint(Input.mousePosition).x, cam.ScreenToWorldPoint(Input.mousePosition).y, 0f);
         Vector3 difference = mousePos - transform.position;
-        
+        float rotationZ = Mathf.Atan2(difference.x, -difference.y) * Mathf.Rad2Deg;
+
+        rb.MoveRotation(Mathf.LerpAngle(rb.rotation, rotationZ, speed * Time.deltaTime));
     }
 }
