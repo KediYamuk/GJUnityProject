300295f7fc1cbf13722e05f16accaa1792582024
Orhun Kaan Çağlayan <66566691+Orhunxx@users.noreply.github.com>
2022-01-27 22:11:30 +0300

    ArmRotating

---
+++ 2D platformerShooterGame/Assets/Scripts/Player/ArmRotate.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmRotate : MonoBehaviour
+{
+    public int isLeftOrRight;
+    public float speed = 300f;
+    private Rigidbody2D rb;
+    public Camera cam;
+
+
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        Vector3 mousePos = new Vector3(cam.ScreenToWorldPoint(Input.mousePosition).x, cam.ScreenToWorldPoint(Input.mousePosition).y, 0f);
+        Vector3 difference = mousePos - transform.position;
+        
+    }
+}
